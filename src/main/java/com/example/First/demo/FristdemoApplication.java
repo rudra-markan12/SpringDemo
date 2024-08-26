3470451719eb1a83e4488cdd3e37c9d638513b57
rudra95314 <rudramarkan0@gmail.com>
2024-08-26 13:45:10 +0530

    first commit

---
+++ src/main/java/com/example/First/demo/FristdemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.First.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FristdemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FristdemoApplication.class, args);
+	}
+
+}
